7edd4ee912323d7aa96198af9a651efabffd735e
Matt Armstrong <matt@roofstock.com>
2015-12-29 22:35:21 -0800

    updated mixpanel script and fixed an error in the instance-initializer.

--- app/instance-initializers/mixpanel.js
+++ app/instance-initializers/mixpanel.js
@@ -2,9 +2,9 @@ import Config from '../config/environment';
 
 export function initialize(instance) {
   if (Config.mixpanel.enabled) {
-    var router = instance.lookup('router:main');
+    var router = instance.container.lookup('router:main');
     router.on('didTransition', function() {
-      instance.lookup('service:mixpanel').trackPageView(this.get('url'));
+      instance.container.lookup('service:mixpanel').trackPageView(this.get('url'));
     });
   }
 }
